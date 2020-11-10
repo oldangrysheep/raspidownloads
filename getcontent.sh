35d6fd9e00e5286603832bc310847926af828407
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-11-10 18:10:19 -0500

    Update getcontent.sh

--- getcontent.sh
+++ getcontent.sh
@@ -1,7 +1,7 @@
 sudo rm -r downloads.txt
-sudo wget 
+sudo rm -r getcontent.sh
+sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/getcontent.sh
 sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads.txt
 sudo wget -i downloads.txt
-sudo rm -r "*.txt"
 sudo unzip "*.zip"
 exit
