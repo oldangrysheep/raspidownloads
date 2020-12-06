fea74e22ff2734f14e8a6ed04bfb87c1a199f54a
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 02:03:43 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-if ! command -v vim &> /dev/null
-then
-    sudo apt-get install fim
-    exit
+if ! [ -x "$(command -v git)" ]; then
+  echo 'Error: git is not installed.' >&2
+  exit 1
 fi
 
 echo -n "Do You want to download all roms?"
