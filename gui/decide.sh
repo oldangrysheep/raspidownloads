a7051bb4476f635952dce9476ef7d77a17fe42b6
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 14:33:52 -0500

    Update decide.sh

--- gui/decide.sh
+++ gui/decide.sh
@@ -1,1 +1,19 @@
-exit
+function advancedMenu {
+    ADVSEL=$(whiptail --title "Decide What Systems You Want" --fb --menu "Choose an option" 15 60 4 \
+        "1" "NES" \
+        "2" "SNES" \
+        "3" "Gameboy" 3>&1 1>&2 2>&3)
+    case $ADVSEL in
+        1)
+            exit
+        ;;
+        2)
+            exit
+        ;;
+        3)
+            echo "Option 3"
+            exit
+        ;;
+    esac
+}
+advancedMenu
