a08f8fd4f4dff7c9b2871abffc5055a1d05891d9
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 03:14:35 -0500

    Update downloader.sh

--- gui/downloader.sh
+++ gui/downloader.sh
@@ -11,50 +11,95 @@ sudo chmod +x downloader.sh
 
 
 
+<<COMMENT
+  WARNING DO NOT USE THIS SCRIPT ON SOMETHING IMPORTANT.
+  IT MAY NOT BE SAFE FOR ONE REASON.
+  1) I AM NOT AN EXPERT AT BASH SCRIPTING.
+  Please i dont want you to mess up your files.
+COMMENT
+
+<<COMMENT
+  If you still wish to use it. note the following things:
+  1) Do not put your script into any of the dirs.
+  2) You need to edit the directory variables so as to
+     fit them to your using.
+     Right now the script presumes that the script is
+     in the same directory as the backup, working and recovery
+     directory.
+  Now feel free to play around or fork it to improve it. cheers :)
+  Feedback is highly welcomed :)
+COMMENT
+
+
+clear
+
+backup_dir="backup"         #The directory that has/will have the backups
+working_dir="working"       #The directory that is to be backed up
+recovery_dir="recovery"     #The directory where you want your recovery to be copied to
+temp_dir="temp"             #A temp directory that the script needs
+
+function backup {
+	{
+    new_backup=B$(date +"%Y%m%d%H%M%S")
+    if [ "$(ls -A $backup_dir)" ]; then
+        mkdir $new_backup
+        rsync -a  $working_dir"/" $temp_dir
+        for entry in "$backup_dir"/*
+        do
+          rm -r $new_backup
+          rsync -a  --compare-dest=../$entry/ $temp_dir/ $new_backup
+          rm -r $temp_dir
+          rsync -a $new_backup/ $temp_dir
+        done
+        mv $new_backup $backup_dir/$new_backup
+        rm -r $temp_dir
+    else
+        rsync -av  $working_dir"/" backup/$new_backup
+    fi
+	} | whiptail --gauge "Backing up data ..." 6 60 0
+}
+
+
+
+function recovery {
+	{
+	  if [ "$(ls -A $backup_dir)" ]; then
+    mkdir $temp_dir
+    for entry in "$backup_dir"/*
+    do
+      rsync -av  --compare-dest=../$temp_dir/ $entry/ $recovery_dir
+      rm -r $temp_dir
+      rsync -av $recovery_dir/ $temp_dir/
+      done
+      rm -r $temp_dir
+    else
+      echo "No backup found"
+    fi
+	} | whiptail --gauge "Recovering data ..." 6 60 0
+}
+
+
+
+while [ 1 ]
+do
 CHOICE=$(
-whiptail --title "Operative Systems" --menu "Make your choice" 16 100 9 \
-	"1)" "The name of this script."   \
-	"2)" "Time since last boot."  \
-	"3)" "Number of processes and threads." \
-	"4)" "Number of context switches in the last secound." \
-	"5)" "How much time used in kernel mode and in user mode in the last secound." \
-	"6)" "Number of interupts in the last secound." \
-	"9)" "End script"  3>&2 2>&1 1>&3	
+whiptail --title "Backup program" --menu "Make your choice" 16 100 9 \
+	"1)" "Backup your files."   \
+	"2)" "Recover your files."  \
+	"9)" "End script"  3>&2 2>&1 1>&3
 )
 
 
-result=$(whoami)
 case $CHOICE in
-	"1)")   
-		echo "hoe"
+	"1)")
+		backup
 	;;
-	"2)")   
-	        OP=$(uptime | awk '{print $3;}')
-		result="This system has been up $OP minutes"
+	"2)")
+	  recovery
 	;;
-
-	"3)")   
-	        p=$(ps ax | wc -l)
-                t=$(ps amx | wc -l)
-		result="Number of processes $p\nNumber os threads $t"
-        ;;
-
-	"4)")   
-	        contextSwitch
-		read -r result < result
-        ;;
-
-	"5)")   
-                userKernelMode
-		read -r result < result
-        ;;
-
-	"6)")   
-		interupts
-		read -r result < result
-        ;;
 
 	"9)") exit
         ;;
 esac
-whiptail --msgbox "$result" 20 78
+done
+exit
