db23f6e7921eb3cdd5ce8eeebd956e98e75111ad
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 18:33:38 -0500

    Update install.sh

--- gui/install.sh
+++ gui/install.sh
@@ -1,1 +1,7 @@
-
+cd /home/pi/RetroPie/retropiemenu
+sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/Rom%20Downloader.sh
+sudo mkdir /home/pi/RomDownloader/Temp
+cd /home/pi/RomDownloader/Temp
+sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/downloader.sh
+cd ~
+sudo rm -r install.sh
