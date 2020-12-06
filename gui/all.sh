7421f15cb512ec80943e3a6228be29780647b25c
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 13:34:52 -0500

    Update all.sh

--- gui/all.sh
+++ gui/all.sh
@@ -1,10 +1,11 @@
-echo -n "Do You want to download all roms?"
-read answer
-if [ "$answer" != "${answer#[Yy]}" ] ;then
+
     wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads.txt
+  
     sudo wget downloads.txt 
+    for i in $(seq 1 100)
+do
+    sleep 0.1 
+    echo $i
+done | whiptail --title 'Test script' --gauge 'Running...' 6 60 0
     sudo rm -r downloads.txt
     sudo unzip *.zip
-else
-    echo No
-fi
