a054386a36cd9ec6dff4d3cf54172c9bb6e2f59b
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 17:11:45 -0500

    Update decide.sh

--- gui/decide.sh
+++ gui/decide.sh
@@ -2,10 +2,19 @@
 echo "yes"
 function advancedMenu {
     ADVSEL=$(whiptail --title "Decide What Systems You Want" --fb --menu "Choose an option" 15 60 4 \
-        "1" "NES" \
-        "2" "SNES" \
+        "1" "Nintendo Entertainment System" \
+        "2" "Super Nintendo Entertainemnt System" \
         "3" "Gameboy" \
-        "4" "Gameboy Color" 3>&1 1>&2 2>&3)
+        "4" "Gameboy Color" \
+        "5" "Gameboy Advanced" \
+        "6" "Nintendo 64" \
+        "7" "Nintendo Ds" \
+        "8" "Nintendo Virtual Boy" \
+        "9" "Megadrive Games" \
+        "10" "Sega Saturn" \ 
+        "11" "Playstation 1" \
+        "12" "Playstation Portable (100+ Gigabytes)" \ 
+        "13" "Sega Dreamcast" 3>&1 1>&2 2>&3)
     case $ADVSEL in
         1)
             exit
