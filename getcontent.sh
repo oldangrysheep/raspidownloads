d35e6457beeeff13b8607d3c59868fda1a760767
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-05 22:46:13 -0500

    Update getcontent.sh

--- getcontent.sh
+++ getcontent.sh
@@ -1,7 +1,14 @@
 sudo rm -r downloads.txt
 sudo rm -r getcontent.sh
-sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/getcontent.sh
-sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads.txt
-sudo wget -i downloads.txt
+sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/g>
+echo -n "Do you want to download all my roms? (Y/N)"
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/ma>
+        sudo wget -i downloads.txt
+
+else
+    echo No
+fi
 sudo unzip *.zip
 exit
