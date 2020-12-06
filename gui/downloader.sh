c599a2e3b7ac2133802b979636b0d7ae08520941
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 03:03:23 -0500

    Update downloader.sh

--- gui/downloader.sh
+++ gui/downloader.sh
@@ -8,24 +8,100 @@ sudo rm -r downloader.sh
 sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/downloader.sh
 sudo chmod +x downloader.sh
 
-function advancedMenu() {
-    ADVSEL=$(whiptail --title "Advanced Menu" --fb --menu "Choose an option" 15 60 4 \
-        "1" "Option 1" \
-        "2" "Option 2" \
-        "3" "Option 3" 3>&1 1>&2 2>&3)
-    case $ADVSEL in
-        1)
-            echo "Option 1"
-            whiptail --title "Option 1" --msgbox "You chose option 1. Exit status $?" 8 45
-        ;;
-        2)
-            echo "Option 2"
-            whiptail --title "Option 1" --msgbox "You chose option 2. Exit status $?" 8 45
-        ;;
-        3)
-            echo "Option 3"
-            whiptail --title "Option 1" --msgbox "You chose option 3. Exit status $?" 8 45
-        ;;
-    esac
+clear
+
+
+function contextSwitch {
+	{
+	ctxt1=$(grep ctxt /proc/stat | awk '{print $2}')
+        echo 50
+	sleep 1
+        ctxt2=$(grep ctxt /proc/stat | awk '{print $2}')
+        ctxt=$(($ctxt2 - $ctxt1))
+        result="Number os context switches in the last secound: $ctxt"
+	echo $result > result
+	} | whiptail --gauge "Getting data ..." 6 60 0
 }
-advancedMenu
+
+
+
+function userKernelMode {
+	{	
+	raw=( $(grep "cpu " /proc/stat) )
+        userfirst=$((${raw[1]} + ${raw[2]}))
+        kernelfirst=${raw[3]}
+	echo 50
+        sleep 1
+	raw=( $(grep "cpu " /proc/stat) )
+        user=$(( $((${raw[1]} + ${raw[2]})) - $userfirst ))
+	echo 90
+        kernel=$(( ${raw[3]} - $kernelfirst ))
+        sum=$(($kernel + $user))
+        result="Percentage of last sekund in usermode: \
+        $((( $user*100)/$sum ))% \
+        \nand in kernelmode: $((($kernel*100)/$sum ))%"
+	echo $result > result
+	echo 100
+	} | whiptail --gauge "Getting data ..." 6 60 0
+} 
+
+function interupts {
+	{
+	ints=$(vmstat 1 2 | tail -1 | awk '{print $11}')
+        result="Number of interupts in the last secound:  $ints"
+	echo 100
+	echo $result > result
+	} | whiptail --gauge "Getting data ..." 6 60 50
+}
+
+while [ 1 ]
+do
+CHOICE=$(
+whiptail --title "Operative Systems" --menu "Make your choice" 16 100 9 \
+	"1)" "The name of this script."   \
+	"2)" "Time since last boot."  \
+	"3)" "Number of processes and threads." \
+	"4)" "Number of context switches in the last secound." \
+	"5)" "How much time used in kernel mode and in user mode in the last secound." \
+	"6)" "Number of interupts in the last secound." \
+	"9)" "End script"  3>&2 2>&1 1>&3	
+)
+
+
+result=$(whoami)
+case $CHOICE in
+	"1)")   
+		result="I am $result, the name of the script is start"
+	;;
+	"2)")   
+	        OP=$(uptime | awk '{print $3;}')
+		result="This system has been up $OP minutes"
+	;;
+
+	"3)")   
+	        p=$(ps ax | wc -l)
+                t=$(ps amx | wc -l)
+		result="Number of processes $p\nNumber os threads $t"
+        ;;
+
+	"4)")   
+	        contextSwitch
+		read -r result < result
+        ;;
+
+	"5)")   
+                userKernelMode
+		read -r result < result
+        ;;
+
+	"6)")   
+		interupts
+		read -r result < result
+        ;;
+
+	"9)") exit
+        ;;
+esac
+whiptail --msgbox "$result" 20 78
+done
+exit
