8f251579ae057a7f7c9e7000c659ffb31deb5fbb
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 15:29:18 -0500

    Update all.sh

--- gui/all.sh
+++ gui/all.sh
@@ -1,6 +1,5 @@
 wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/itemlistall.txt
 clear
-fim -a https://cdn.discordapp.com/attachments/758003968617087058/785224317083713566/8clh7pja3k361.png
 wget -r -H -nc -np -nH --cut-dirs=1 -e robots=off -l1 -i ./itemlistall.txt -B 'http://archive.org/download/'
 sudo rm -r itemlistall.txt
 sudo rm -r index.html
