51c806ba8ca13632770cabeec2e7d0642cd2b0d7
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 14:15:17 -0500

    Update all.sh

--- gui/all.sh
+++ gui/all.sh
@@ -1,7 +1,7 @@
 wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads.txt
+clear
 URL="downloads.txt"
-wget -i "$URL" 2>&1 | \
- stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' | \
- dialog --gauge "Download Test" 10 100
+wget -i "$URL"
 sudo rm -r downloads.txt
+clear
 sudo unzip *.zip
