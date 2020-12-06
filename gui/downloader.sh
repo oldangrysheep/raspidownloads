2c9179f0c3bf35f6cc661dc4f13ec101fa7a1c0f
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 02:56:56 -0500

    Update downloader.sh

--- gui/downloader.sh
+++ gui/downloader.sh
@@ -1,3 +1,9 @@
+#This is all setup stuff to make sure its all ready
+if ! [ -x "$(command -v whiptail)" ]; then
+  echo "Just installing Dependancies" 
+  sudo apt-get install whiptail -y
+fi
+
 sudo rm -r downloader.sh
 sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/downloader.sh
 sudo chmod +x downloader.sh
