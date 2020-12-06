ddd81301d23ab1b83704dbf6d89c14cf51dc8d3b
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 13:30:30 -0500

    Update all.sh

--- gui/all.sh
+++ gui/all.sh
@@ -1,1 +1,10 @@
-
+echo -n "Do You want to download all roms?"
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads.txt
+    sudo wget downloads.txt 
+    sudo rm -r downloads.txt
+    sudo unzip *.zip
+else
+    echo No
+fi
