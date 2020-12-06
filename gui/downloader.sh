5ece7d538845e2f378bea2063e8bc9fd00301d27
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 02:54:07 -0500

    Update downloader.sh

--- gui/downloader.sh
+++ gui/downloader.sh
@@ -2,11 +2,24 @@ sudo rm -r downloader.sh
 sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/downloader.sh
 sudo chmod +x downloader.sh
 
-if (whiptail --title "Example Dialog" --yesno "This is an example of a yes/no box." 8 78); then
-    whiptail --title "Menu example" --menu "Choose an option" 25 78 16 \
-    "<-- Back" "Return to the main menu." \
-    "Add User" "Add a user to the system." \
-    "Modify User" "Modify an existing user." \
-else
-    echo "User selected No, exit status was $?."
-fi
+function advancedMenu() {
+    ADVSEL=$(whiptail --title "Advanced Menu" --fb --menu "Choose an option" 15 60 4 \
+        "1" "Option 1" \
+        "2" "Option 2" \
+        "3" "Option 3" 3>&1 1>&2 2>&3)
+    case $ADVSEL in
+        1)
+            echo "Option 1"
+            whiptail --title "Option 1" --msgbox "You chose option 1. Exit status $?" 8 45
+        ;;
+        2)
+            echo "Option 2"
+            whiptail --title "Option 1" --msgbox "You chose option 2. Exit status $?" 8 45
+        ;;
+        3)
+            echo "Option 3"
+            whiptail --title "Option 1" --msgbox "You chose option 3. Exit status $?" 8 45
+        ;;
+    esac
+}
+advancedMenu
