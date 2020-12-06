e153121a4f9fe309eeae7daec08007b0dbc4c1a7
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 02:48:25 -0500

    Update downloader.sh

--- gui/downloader.sh
+++ gui/downloader.sh
@@ -3,7 +3,10 @@ sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gu
 sudo chmod +x downloader.sh
 
 if (whiptail --title "Example Dialog" --yesno "This is an example of a yes/no box." 8 78); then
-    echo "User selected Yes, exit status was $?."
+    whiptail --title "Menu example" --menu "Choose an option" 25 78 16 \
+    "<-- Back" "Return to the main menu." \
+    "Add User" "Add a user to the system." \
+    "Modify User" "Modify an existing user." \
 else
     echo "User selected No, exit status was $?."
 fi
