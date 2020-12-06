347056e00e8599f6d0e22e04d8417242507451ed
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 13:38:51 -0500

    Update all.sh

--- gui/all.sh
+++ gui/all.sh
@@ -2,10 +2,10 @@
     wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads.txt
   
     sudo wget downloads.txt 
-    for i in $(seq 1 100)
-do
-    sleep 0.1 
-    echo $i
-done | whiptail --title 'Downloading Roms' --gauge 'Downloading...' 6 60 0
+    #for i in $(seq 1 100)
+#do
+ #   sleep 0.1 
+#    echo $i
+#done | whiptail --title 'Downloading Roms' --gauge 'Downloading...' 6 60 0
     sudo rm -r downloads.txt
     sudo unzip *.zip
