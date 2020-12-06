d662cc15f888d4f2972ddfaaab67605814c6bba2
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 02:30:05 -0500

    Update downloader.sh

--- gui/downloader.sh
+++ gui/downloader.sh
@@ -1,1 +1,3 @@
-
+sudo rm -r downloader.sh
+sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/downloader.sh
+whiptail --title "Example Dialog" --infobox "This is an example of an info box." 8 78
