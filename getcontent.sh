906bc88954287264a2a9e83fd94514a538267f73
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-05 22:56:38 -0500

    Update getcontent.sh

--- getcontent.sh
+++ getcontent.sh
@@ -1,14 +1,20 @@
 sudo rm -r downloads.txt
 sudo rm -r getcontent.sh
-sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/g>
+sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/getcontent.sh
 echo -n "Do you want to download all my roms? (Y/N)"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/ma>
+    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads.txt
         sudo wget -i downloads.txt
-
+else
+    echo -n "Do You want to download all nes roms y/n?"
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    echo Yes
 else
     echo No
+fi
+
 fi
 sudo unzip *.zip
 exit
