ac6c811ff378238a7cf6125207c1d6ce0ce60aaa
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 15:00:54 -0500

    Update all.sh

--- gui/all.sh
+++ gui/all.sh
@@ -2,5 +2,6 @@ wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/ite
 clear
 wget -r -H -nc -np -nH --cut-dirs=1 -e robots=off -l1 -i ./itemlistall.txt -B 'http://archive.org/download/'
 sudo rm -r itemlistall.txt
+sudo rm -r index.html
 clear
 sudo unzip *.zip
