aec42fe064d24df359f24f24e169e6470472d7b5
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 02:36:54 -0500

    Update downloader.sh

--- gui/downloader.sh
+++ gui/downloader.sh
@@ -1,4 +1,9 @@
 sudo rm -r downloader.sh
 sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/downloader.sh
 sudo chmod +x downloader.sh
-whiptail --title "Example Dialog" --msgbox "This is an example of a message box. You must hit OK to continue." 8 78
+
+if (whiptail --title "Example Dialog" --yesno "This is an example of a yes/no box." 8 78); then
+    echo "User selected Yes, exit status was $?."
+else
+    echo "User selected No, exit status was $?."
+fi
