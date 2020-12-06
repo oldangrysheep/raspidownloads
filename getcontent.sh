bc9f18c522b3d1499df04d722154020904ac6866
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 00:48:34 -0500

    Update getcontent.sh

--- getcontent.sh
+++ getcontent.sh
@@ -2,13 +2,7 @@ sudo rm -r downloads.txt
 sudo rm -r getcontent.sh
 sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/getcontent.sh
 chmod +x getcontent.sh
-echo -n "Do you want to download all my roms? (Y/N)"
-read answer
-if [ "$answer" != "${answer#[Yy]}" ] ;then
-    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads.txt
-        sudo wget -i downloads.txt
-else
-    echo -n "Do You want to download all nes roms y/n?"
+echo -n "Do You want to download all nes roms y/n?"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
     echo "https://ia801402.us.archive.org/zip_dir.php?path=/13/items/nes_games_2000_raspi.zip" >> downloads.txt
@@ -27,7 +21,7 @@ fi
     echo -n "Do You want to download all gameboy roms y/n?"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads/gameboy.txt
+    echo "https://ia801505.us.archive.org/zip_dir.php?path=/22/items/gameboy_games_2000_raspi.zip" >> downloads.txt
 else
     echo No
 fi
@@ -35,7 +29,7 @@ fi
     echo -n "Do You want to download all gameboy advanced roms y/n?"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads/gba.txt
+    echo "https://ia801504.us.archive.org/zip_dir.php?path=/3/items/gba_games_2000_raspi.zip" >> downloads.txt
 else
     echo No
 fi
@@ -43,7 +37,7 @@ fi
     echo -n "Do You want to download all gbc roms y/n?"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads/gbc.txt
+    echo "https://ia801504.us.archive.org/zip_dir.php?path=/24/items/gbc_games_2000_raspi.zip"
 else
     echo No
 fi
@@ -51,7 +45,7 @@ fi
     echo -n "Do You want to download all megadrive roms y/n?"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads/megadrive.txt
+    echo "https://ia801404.us.archive.org/zip_dir.php?path=/6/items/megadrive_games_2000_raspi.zip" >> downloads.txt
 else
     echo No
 fi
@@ -61,7 +55,7 @@ fi
     echo -n "Do You want to download all Nintendo 64 roms y/n?"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads/n64.txt
+    echo "https://ia601406.us.archive.org/zip_dir.php?path=/24/items/n64_games_2000_raspi.zip" >> downloads.txt
 else
     echo No
 fi
@@ -69,7 +63,7 @@ fi
     echo -n "Do You want to download all Nintendo ds roms y/n?"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads/nds.txt
+    echo "https://ia601500.us.archive.org/zip_dir.php?path=/25/items/nds_games_2000_raspi.zip" >> downloads.txt
 else
     echo No
 fi
@@ -77,15 +71,15 @@ fi
     echo -n "Do You want to download all Nintendo Entertainment Sytstem (NES) Roms Y/N"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads/nes.txt
+    echo "https://ia801402.us.archive.org/zip_dir.php?path=/13/items/nes_games_2000_raspi.zip" >> downloads.txt
 else
     echo No
 fi
 
-    echo -n "Do You want to download all Playstation 3 roms y/n? (Not Reccomended 100+ Gigabytes "
+    echo -n "Do You want to download all Playstation 3 roms y/n? (Not Reccomended 100+ Gigabytes & not playable on raspi "
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads/ps3.txt
+    echo "https://archive.org/download/ps-3-disc/ps3/" >> downloads.txt
 else
     echo No
 fi
@@ -93,7 +87,7 @@ fi
     echo -n "Do You want to download all Playstation Portable roms y/n?"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads/psp.txt
+    echo "https://archive.org/download/psp_temporary/mnt/NAS/raspi/psp/" >> downloads.txt
 else
     echo No
 fi
@@ -101,7 +95,7 @@ fi
     echo -n "Do You want to download all Playstaion 1 roms y/n?"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads/psx.txt
+    echo "https://ia801502.us.archive.org/29/items/psx_gamescollection/psx%20all%20games.zip" >> downloads.txt
 else
     echo No
 fi
@@ -109,7 +103,7 @@ fi
     echo -n "Do You want to download all Sega Saturn roms y/n?"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads/saturn.txt
+    echo "https://ia601402.us.archive.org/zip_dir.php?path=/28/items/saturn_games_2000_raspi.zip" >> downloads.txt
 else
     echo No
 fi
@@ -118,7 +112,7 @@ fi
 
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads/snes.txt
+    echo "https://ia601508.us.archive.org/zip_dir.php?path=/5/items/snes_games_2000_raspi.zip" >> downloads.txt
 else
     echo No
 fi
@@ -126,14 +120,13 @@ fi
     echo -n "Do You want to download all virtual boy roms y/n?"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads/virtualboy.txt
-else
+    echo "https://ia801400.us.archive.org/zip_dir.php?path=/31/items/virtualboy_games_2000_raspi.zip" >> downloads.txt
+else 
     echo No
 fi
 
 sudo wget -i downloads.txt
 
-fi
 sudo unzip *.zip
 sudo rm -r downloads.txt
 
