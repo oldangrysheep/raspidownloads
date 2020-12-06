d9149e1081bb680e00e6e17a6d4e42377473ad96
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-06 01:00:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-echo -n "Do You want to download all roms?
+echo -n "Do You want to download all roms?"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
     wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/downloads.txt
@@ -9,7 +9,7 @@ else
     echo No
 fi
 
-echo -n "Do You want to decide which roms you get??
+echo -n "Do You want to decide which roms you get??"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
     wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/getcontent.sh
@@ -19,3 +19,4 @@ else
 fi
 sudo rm -r install.sh
 sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/install.sh
+sudo chmod +x install.sh
