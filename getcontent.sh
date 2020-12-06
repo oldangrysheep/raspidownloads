daf0fe934fe4d65dbd6c639b4ea3b220004fd50b
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-05 23:00:46 -0500

    Update getcontent.sh

--- getcontent.sh
+++ getcontent.sh
@@ -1,6 +1,7 @@
 sudo rm -r downloads.txt
 sudo rm -r getcontent.sh
 sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/getcontent.sh
+chmod +x getcontent.sh
 echo -n "Do you want to download all my roms? (Y/N)"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
