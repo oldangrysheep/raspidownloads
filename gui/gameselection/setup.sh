9d3820f6a7d4acf64c314fecb92903bb39fbfaa7
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-07 14:02:57 -0500

    Update setup.sh

--- gui/gameselection/setup.sh
+++ gui/gameselection/setup.sh
@@ -2,6 +2,24 @@ cd /home/pi/RomDownloader/Temp/Roms
 sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/DownloadSelected.sh
 sudo chmod +x DownloadSelected.sh
 sudo mkdir -p Items_To_Download
+cd /home/pi/RomDownloader/Temp/Roms/Items_To_Download
+sudo mkdir -p nes
+sudo mkdir -p snes
+sudo mkdir -p n64
+sudo mkdir -p gb
+sudo mkdir -p gbc
+sudo mkdir -p gba
+sudo mkdir -p nds
+sudo mkdir -p virtualboy
+sudo mkdir -p atari2600
+sudo mkdir -p atari5200
+sudo mkdir -p atari7800
+sudo mkdir -p megadrive
+sudo mkdir -p saturn
+sudo mkdir -p dreamcast
+sudo mkdir -p psx
+sudo mkdir -p psp
+cd /home/pi/RomDownloader/Temp/Roms/
 sudo mkdir -p nes
 sudo mkdir -p snes
 sudo mkdir -p n64
