83e2c9e69e3f57dafa7e4e6ded9ef4eeeab5aed8
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-07 01:20:23 -0500

    Update decide2.sh

--- gui/decide2.sh
+++ gui/decide2.sh
@@ -1,1 +1,6 @@
-
+whiptail --title "How To Use" --msgbox "Click ok on every system you want then click download" 8 45
+sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/decide.sh
+sudo chmod +x decide.sh
+wait 5
+sudo sh ./decide.sh
+sudo rm -r decide.sh
