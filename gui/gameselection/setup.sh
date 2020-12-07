68ad489b69aa4b47c210b18f9078b3738567c64d
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-07 11:15:42 -0500

    Update setup.sh

--- gui/gameselection/setup.sh
+++ gui/gameselection/setup.sh
@@ -1,3 +1,4 @@
 sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/gamelist.sh
+sudo chmod +x gamelist.sh
 sudo bash ./gamelist.sh
 sudo rm -r gamelist.sh
