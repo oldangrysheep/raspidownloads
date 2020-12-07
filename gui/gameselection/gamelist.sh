15c0a7b9163fbeccf3f0230bb881007225c3f3d8
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-07 09:26:39 -0500

    Update gamelist.sh

--- gui/gameselection/gamelist.sh
+++ gui/gameselection/gamelist.sh
@@ -1,1 +1,12 @@
-
+i=0
+s=65    # decimal ASCII "A" 
+for f in *.tgz
+do
+    # convert to octal then ASCII character for selection tag
+    files[i]=$(echo -en "\0$(( $s / 64 * 100 + $s % 64 / 8 * 10 + $s % 8 ))")
+    files[i+1]="$f"    # save file name
+    ((i+=2))
+    ((s++))
+done
+whiptail --backtitle "Welcome to SEUL" --title "Restore Files" \
+    --menu "Please select the file to restore" 14 40 6 "${files[@]}"
