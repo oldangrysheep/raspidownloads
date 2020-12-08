01756f4250dd231a82b4531c17c7506063d0bb2f
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-08 18:27:24 -0500

    Update DownloadSelected.sh

--- gui/gameselection/DownloadSelected.sh
+++ gui/gameselection/DownloadSelected.sh
@@ -2,4 +2,4 @@ cd /home/pi/RomDownloader/Temp/roms/Items_To_Download/
 cd /home/pi/RomDownloader/Temp/roms/Items_To_Download/nes
 sudo wget -r -H -nc -np -nH --cut-dirs=1 -e robots=off -l1 -P /home/pi/RomDownloader/Roms/Downloaded -i ./itemlist.txt -B 'http://archive.org/download/'
 sudo rm -r itemlist.txt
-
+sudo mv /home/pi/RomDownloader/Temp/roms/Items_To_Download/* /home/pi/RetroPie/roms
