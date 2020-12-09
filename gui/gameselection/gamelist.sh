62d9a28cad3f97aae1ccdf0e0e94d84072e0ae1d
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-09 02:26:54 -0500

    Update gamelist.sh

--- gui/gameselection/gamelist.sh
+++ gui/gameselection/gamelist.sh
@@ -7,6 +7,16 @@ for i in $(seq 0 $((${#files[@]}-1))); do
     echo "$i) ${files[$i]}" 
 done
 
+whiptail --title "Select File" --backtitle "File Selector" --menu "Select the file from the list below:" 30 80 24 "{files[@]}" 2>&1 >/dev/tty)
+if [[ $? != 0 ]]
+then
+    echo "Cancelled!"
+    exit 1
+fi
+echo "$result"
+
+echo "$
+
 read -e -p "Select a file by index from the list: " -i 1 choice
 
 # TODO validate and default choice
