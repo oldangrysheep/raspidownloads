467826e7f41202543e01e892101535914848e19a
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-09 02:27:50 -0500

    Update gamelist.sh

--- gui/gameselection/gamelist.sh
+++ gui/gameselection/gamelist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 shopt -s nullglob
 cd /home/pi/RomDownloader/Temp/Roms/nes
-array=(*.png)
+array=(*.sh)
 # Create new array with null string ("") item after each filename item, so whiptail has two strings to read for each filename.
 for ((i=0; i<${#arr[@]}; i++)); do j=$((2*$i)); a[j]="${arr[$i]}"; a[j+1]=""; done
 result=$(whiptail --title "Select File" --backtitle "File Selector" --menu "Select the file from the list below:" 30 80 24 "${a[@]}" 2>&1 >/dev/tty)
