db2ae922b9d1042bf05d14f7c8c51c6c04469b53
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-09 10:24:48 -0500

    Update gamelist.sh

--- gui/gameselection/gamelist.sh
+++ gui/gameselection/gamelist.sh
@@ -8,12 +8,12 @@ for ((i=0; i<${#arr[@]}; i++)); do j=$((2*$i)); a[j]="${arr[$i]}"; a[j+1]=""; do
 result=$(whiptail --title "Select File" --backtitle "File Selector" --menu "Select the file from the list below:" 30 80 24 "${a[@]}" 3>&1 1>&2 2>&3)
 if [[ $? != 0 ]]
 then
-    echo "Cancelled!"
     echo "$i"
     echo "${a[@]}"
     echo "$a"
     echo "${arr[$i]}"
     echo "$result"
+    echo "Cancelled!"
     exit 1
 fi
 echo "$result"
