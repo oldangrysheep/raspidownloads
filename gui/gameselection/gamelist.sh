d0db098d553388748c5598583f4bd1338efde9ce
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-09 02:01:43 -0500

    Update gamelist.sh

--- gui/gameselection/gamelist.sh
+++ gui/gameselection/gamelist.sh
@@ -3,12 +3,10 @@ i=0
 s=65    # decimal ASCII "A" 
 for f in *.sh
 do
-    # convert to octal then ASCII character for selection tag
-    files[i]=$(echo -en "\0$(( $s / 64 * 100 + $s % 64 / 8 * 10 + $s % 8 ))")
-    files[i+1]="$f"    # save file name
-    ((i+=2))
-    ((s++))
+    echo "$files"
 done
+
+
 
 whiptail --backtitle "Welcome to SEUL" --title "Restore Files" \
     --menu "Please select the file to restore" 14 40 6 "${files[@]}"
