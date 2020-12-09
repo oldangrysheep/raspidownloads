6113732b58d3efc358166b149ee0f01116383585
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-09 18:12:20 -0500

    Update gamelist.sh

--- gui/gameselection/gamelist.sh
+++ gui/gameselection/gamelist.sh
@@ -12,6 +12,4 @@ done
 whiptail --backtitle "Welcome to SEUL" --title "Restore Files" \
     --menu "Please select the file to restore" 14 40 6 "${files[@]}"
 
-result=$(whiptail-command 3>&2 2>&1 1>&3-)
-
 ((index = 2 * ( $( printf "%d" "'$result" ) - 65 ) + 1 ))
