c4e37175f7986608da3fa1f1c9d978114abae4c3
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-09 02:34:03 -0500

    Update gamelist.sh

--- gui/gameselection/gamelist.sh
+++ gui/gameselection/gamelist.sh
@@ -1,13 +1,24 @@
-#!/bin/bash
-shopt -s nullglob
 cd /home/pi/RomDownloader/Temp/Roms/nes
-array=(*.sh)
-# Create new array with null string ("") item after each filename item, so whiptail has two strings to read for each filename.
-for ((i=0; i<${#arr[@]}; i++)); do j=$((2*$i)); a[j]="${arr[$i]}"; a[j+1]=""; done
-result=$(whiptail --title "Select File" --backtitle "File Selector" --menu "Select the file from the list below:" 30 80 24 "${a[@]}" 2>&1 >/dev/tty)
+
+
+files=(*.sh)
+
+for i in $(seq 0 $((${#files[@]}-1))); do 
+    echo "$i) ${files[$i]}" 
+done
+
+whiptail --title "Select File" --backtitle "File Selector" --menu "Select the file from the list below:" 30 80 24 "{files[@]}" 2>&1 >/dev/tty)
 if [[ $? != 0 ]]
 then
     echo "Cancelled!"
     exit 1
 fi
 echo "$result"
+
+echo "$
+
+read -e -p "Select a file by index from the list: " -i 1 choice
+
+# TODO validate and default choice
+
+echo "File choice ${files[$choice]}"
