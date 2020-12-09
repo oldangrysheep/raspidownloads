5f51621f79c3547a3d48e02acab744143f9ecd8f
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-09 02:14:02 -0500

    Update gamelist.sh

--- gui/gameselection/gamelist.sh
+++ gui/gameselection/gamelist.sh
@@ -1,10 +1,17 @@
 cd /home/pi/RomDownloader/Temp/Roms/nes
-IFS=', ' read -r -a array <<< "$string"
-echo "${array[0]}"
-
+i=0
+s=65    # decimal ASCII "A" 
+for f in *.sh
+do
+    # convert to octal then ASCII character for selection tag
+    files[i]=$(echo -en "\0$(( $s / 64 * 100 + $s % 64 / 8 * 10 + $s % 8 ))")
+    files[i+1]="$f"    # save file name
+    ((i+=2))
+    ((s++))
+done
 
 whiptail --backtitle "Welcome to SEUL" --title "Restore Files" \
-    --menu "Please select the file to restore" 14 40 6 "${array[0]}"
+    --menu "Please select the file to restore" 14 40 6 "${files[@]}"
     
     result=$(whiptail-command 3>&2 2>&1 1>&3-)
     
