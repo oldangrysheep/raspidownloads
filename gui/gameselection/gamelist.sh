2c2d0f561f5e3a25dbbc4e7bdae57d8a99e08a7f
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-09 09:51:40 -0500

    Update gamelist.sh

--- gui/gameselection/gamelist.sh
+++ gui/gameselection/gamelist.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 cd /home/pi/RomDownloader/Temp/Roms/nes
-files=(*.sh)
 
-for i in $(seq 0 $((${#files[@]}-1))); do 
-    whiptail --title "Select File" --backtitle "File Selector" --menu "Select the file from the list below:" 30 80 24 "${files[$i]" 2>&1 >/dev/tty) 
-done
-
-read -e -p "Select a file by index from the list: " -i 1 choice
-
-# TODO validate and default choice
-
-echo "File choice ${files[$choice]}"
-exit
+read -ra array <<<$(ifconfig -a | awk '!/^ / && NF {print $1; print $1}')
+whiptail --title Networking --menu "select your choice" 16 78 5 "${array[@]}"
