b33556322beb8cfe957225f023481adbd7eaac67
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-09 02:17:35 -0500

    Update gamelist.sh

--- gui/gameselection/gamelist.sh
+++ gui/gameselection/gamelist.sh
@@ -1,23 +1,14 @@
 cd /home/pi/RomDownloader/Temp/Roms/nes
 
-saveIFS="$IFS"
-IFS=$'\n'
-array=($(<Name.txt))
-IFS="$saveIFS"
-echo ""
- 
-for i in "${array[@]}"; do echo "$i"; done # iterate over the array
-echo "These are my Varibles above " 
-echo "" 
-#This is where the Wiptail script comes in..passing in the array ${array[@]}
-whiptail --backtitle "Hi" --title "These are the names" --menu "Please select the name you like" 14 40 6 "${array[@]}"
-#This shows exit status
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-echo "User selected Ok and entered " 
- 
-#This is where I would like to echo out the value selected
-#echo value
-else
-echo "User selected Cancel."
-fi
+
+files=(*.sh)
+
+for i in $(seq 0 $((${#files[@]}-1))); do 
+    echo "$i) ${files[$i]}" 
+done
+
+read -e -p "Select a file by index from the list: " -i 1 choice
+
+# TODO validate and default choice
+
+echo "File choice ${files[$choice]}"
