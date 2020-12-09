2179dc1dc8025f4d4ab9368b26180b338f355b0c
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-09 02:43:09 -0500

    Update gamelist.sh

--- gui/gameselection/gamelist.sh
+++ gui/gameselection/gamelist.sh
@@ -1,21 +1,12 @@
+#!/bin/bash
+
 cd /home/pi/RomDownloader/Temp/Roms/nes
 
-
-files=(*.sh)
+files=(*.sh
 
 for i in $(seq 0 $((${#files[@]}-1))); do 
     echo "$i) ${files[$i]}" 
 done
-
-whiptail --title "Select File" --backtitle "File Selector" --menu "Select the file from the list below:" 30 80 24 "{files[@]}" 2>&1 >/dev/tty)
-if [[ $? != 0 ]]
-then
-    echo "Cancelled!"
-    exit 1
-fi
-echo "$result"
-
-echo "$
 
 read -e -p "Select a file by index from the list: " -i 1 choice
 
