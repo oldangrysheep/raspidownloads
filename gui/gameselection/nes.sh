fec22e3d8327e01f5e3e9497f26082fdf5eddd93
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-09 22:14:11 -0500

    Update nes.sh

--- gui/gameselection/nes.sh
+++ gui/gameselection/nes.sh
@@ -1,26 +1,17 @@
+#! /bin/bash
+shopt -s nullglob
+dir=`pwd`
 cd /home/pi/RomDownloader/Temp/Roms/nes
-i=0
-s=65    # decimal ASCII "A" 
-for f in *.sh
-do
-    # convert to octal then ASCII character for selection tag
-    files[i]=$(echo -en "\0$(( $s / 64 * 100 + $s % 64 / 8 * 10 + $s % 8 ))")
-    files[i+1]="$f"    # save file name
-    ((i+=2))
-    ((s++))
-done
-
-whiptail --backtitle "Welcome to SEUL" --title "Restore Files" \
-    --menu "Please select the file to restore" 14 40 6 "${files[@]}"
-    
-    result=$(whiptail-command 3>&2 2>&1 1>&3-)
-    
-    ((index = 2 * ( $( printf "%d" "'$result" ) - 65 ) + 1 ))
-
-
+arr=(*.sh)
+for ((i=0; i<${#arr[@]}; i++)); do j=$((2*$i+2)); a[j]="${arr[$i]}"; a[j+1]=""; done
+a[0]=""
+# Next line has extra spaces at right to try to center it:
+a[1]="Please make a selection from the files below                                                        "
+result=$(whiptail --ok-button "OK button text" --cancel-button "Cancel Button Text" --title "Title Text" --backtitle "Text at upper left corner of page" --menu "Menu Text (not used??)" 30 160 24 "${a[@]}" 2>&1 >/dev/tty)
+if [[ $? = 0 ]]
+then
+# ge 5 in next line should be length of file extension including . character, plus 1
+  [ ${#result} -ge 5 ] && outfile="/path/to/files/$result" || echo "Selection not made"
+fi
 echo "$result"
-echo "$f"
-echo "$files"
-echo "$s++"
-echo "Updated text"
-
+cd "$dir"
