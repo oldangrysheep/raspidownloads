916761acadc6a73211bc3d8c0a08ccd25f3efb05
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-09 23:20:28 -0500

    Update gamelist.sh

--- gui/gameselection/gamelist.sh
+++ gui/gameselection/gamelist.sh
@@ -1,17 +1,88 @@
-#! /bin/bash
-shopt -s nullglob
-dir=`pwd`
-cd /home/pi/RomDownloader/Temp/Roms/nes
-arr=(*.sh)
-for ((i=0; i<${#arr[@]}; i++)); do j=$((2*$i+2)); a[j]="${arr[$i]}"; a[j+1]=""; done
-a[0]=""
-# Next line has extra spaces at right to try to center it:
-a[1]="Please make a selection from the files below                                                        "
-result=$(whiptail --ok-button "OK button text" --cancel-button "Cancel Button Text" --title "Title Text" --backtitle "Text at upper left corner of page" --menu "Menu Text (not used??)" 30 160 24 "${a[@]}" 2>&1 >/dev/tty)
-if [[ $? = 0 ]]
-then
-# ge 5 in next line should be length of file extension including . character, plus 1
-  [ ${#result} -ge 5 ] && outfile="/path/to/files/$result" || echo "Selection not made"
+
+function advancedMenu {
+    ADVSEL=$(whiptail --title "Select The System You Want To Select The Games From" --menu "Choose an option" 15 60 4 \
+        "1" "Nintendo Entertainment System" \
+        "2" "Super Nintendo Entertainemnt System" \
+        "3" "Gameboy" \
+        "4" "Gameboy Color" \
+        "5" "Gameboy Advanced" \
+        "6" "Nintendo 64" \
+        "7" "Nintendo Ds" \
+        "8" "Nintendo Virtual Boy" \
+        "9" "Megadrive Games" \
+        "10" "Sega Saturn" \
+        "11" "Sega Dreamcast" \
+        "12" "Playstation 1" \
+        "13" "Playstation Portable" \
+        "14" "Download All Selected" 3>&1 1>&2 2>&3)
+    case $ADVSEL in
+        1)
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/nes.sh
+            sudo sh ./nes.sh
+        ;;
+        2)
+            echo "https://archive.org/download/gb_20201207/Complete%20Rom%20Sets/snes.zip" >> itemlist.txt
+            sudo sh ./decide.sh
+        ;;
+        3)
+            echo "https://archive.org/download/gb_20201207/Complete%20Rom%20Sets/gb.zip" >> itemlist.txt
+            sudo sh ./decide.sh
+        ;;
+        4)
+            echo "https://archive.org/download/gb_20201207/Complete%20Rom%20Sets/gbc.zip" >> itemlist.txt
+            sudo sh ./decide.sh
+        ;;
+        5)
+            echo "https://archive.org/download/gb_20201207/games/gba.zip" >> itemlist.txt
+            sudo sh ./decide.sh
+        ;;
+        6)
+            exit
+        ;;
+        7)
+            echo "https://archive.org/download/gb_20201207/games/nds.zip" >> itemlist.txt
+            sudo sh ./decide.sh
+        ;;
+        8)
+            echo "https://archive.org/download/gb_20201207/Complete%20Rom%20Sets/virtualboy.zip" >> itemlist.txt
+            sudo sh ./decide.sh
+        ;;
+        9)
+            echo "https://archive.org/download/gb_20201207/games/megadrive.zip" >> itemlist.txt
+            sudo sh ./decide.sh
+        ;;
+        10)
+            echo "https://archive.org/download/gb_20201207/games/saturn.zip" >> itemlist.txt
+            sudo sh ./decide.sh
+        ;;
+        11)
+            exit
+        ;;
+        12)
+            exit
+        ;;
+        13)
+            exit
+        ;;
+        14)
+            if (whiptail --title "Are You Sure?" --yes-button "Download" --yesno "Yes Will Download, No Will Bring You Back To The Menu." 10 60) then
+    sudo wget -r -H -nc -np -nH --cut-dirs=1 -e robots=off -l1 -i ./itemlist.txt -B 'http://archive.org/download/'
+    cd /home/pi/RomDownloader/Temp/gb_20201207/games
+    for i in {1..100}; do
+   echo $i
+   sleep 0.1
+done | whiptail --gauge "Extracting Files ..." 10 50 0
+    sudo unzip '*.zip'
+    sudo rm -r *.zip
+    sudo cp -a /home/pi/RomDownloader/Temp/gb_20201207/games/* cd /home/pi/RetroPie/roms/
+    whiptail --title "Your Downloads Are Done" --msgbox "Please restart emulation station to see the games." 8 45
+    cd /home/pi/RomDownloader/Temp
+else
+    sudo sh ./decide.sh
 fi
-echo "$result"
-cd "$dir"
+            
+            
+        ;;
+    esac
+}
+advancedMenu
