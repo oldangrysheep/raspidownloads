5d3a3e3ce714a10b0bdd991dffbf099270ba5b4a
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-09 23:34:10 -0500

    Update setup.sh

--- gui/gameselection/setup.sh
+++ gui/gameselection/setup.sh
@@ -28,4 +28,4 @@ sudo chmod -R +x *.sh
 cd /home/pi/RomDownloader/Temp/
 sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/gamelist.sh
 sudo chmod +x gamelist.sh
-sudo bash gamelist.sh
+sudo sh gamelist.sh
