ebd7e1ff70c1a2968ea9f576b6e0cec22a92937e
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-10 00:36:28 -0500

    Update Download The Items You Selected.sh

--- gui/gameselection/Download The Items You Selected.sh
+++ gui/gameselection/Download The Items You Selected.sh
@@ -1,5 +1,5 @@
 pwd
-cd /home/pi/RomDownloader/Temp
+cd /home/pi/RomDownloader/Temp/Roms
 sudo wget -r -H -nc -np -nH --cut-dirs=1 -e robots=off -l1 -P /home/pi/RomDownloader/Roms/Downloaded -i ./itemlist.txt -B 'http://archive.org/download/'
 sudo rm -r itemlist.txt
 sudo mv /home/pi/RomDownloader/Temp/roms/Items_To_Download/* /home/pi/RetroPie/roms
