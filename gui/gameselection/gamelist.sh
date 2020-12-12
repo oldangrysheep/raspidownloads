d1781eafd2767e30f76d9656caaf8317cbcf997b
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-11 23:22:33 -0500

    Update gamelist.sh

--- gui/gameselection/gamelist.sh
+++ gui/gameselection/gamelist.sh
@@ -1,79 +1,124 @@
 
 function advancedMenu {
     ADVSEL=$(whiptail --title "Select The System You Want To Select The Games From" --menu "Choose an option" 15 60 4 \
-        "1" "Nintendo Entertainment System" \
-        "2" "Super Nintendo Entertainemnt System" \
-        "3" "Gameboy" \
-        "4" "Gameboy Color" \
-        "5" "Gameboy Advanced" \
-        "6" "Nintendo 64" \
-        "7" "Nintendo Ds" \
-        "8" "Nintendo Virtual Boy" \
-        "9" "Megadrive Games" \
-        "10" "Sega Saturn" \
-        "11" "Sega Dreamcast" \
-        "12" "Playstation 1" \
-        "13" "Playstation Portable" \
-        "14" "Download All Selected" 3>&1 1>&2 2>&3)
+        "1" "Download All Selected" \
+        "2" "Download Selected In Backround" \
+        "3" "Nintendo Entertainment System" \
+        "4" "Super Nintendo Entertainemnt System" \
+        "5" "Gameboy" \
+        "6" "Gameboy Color" \
+        "7" "Gameboy Advanced" \
+        "8" "Nintendo 64" \
+        "9" "Nintendo Ds" \
+        "10" "Nintendo Virtual Boy" \
+        "11" "Megadrive Games" \
+        "12" "Sega Saturn" \
+        "13" "Sega Dreamcast" \
+        "14" "Playstation 1" \
+        "15" "Playstation Portable" 3>&1 1>&2 2>&3)
     case $ADVSEL in
         1)
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/Download%20The%20Items%20You%20Selected.sh
+            sudo chmod +x "Download The Items You Selected.sh"
+            sudo sh "Download The Items You Selected.sh"
+            whiptail --title "Downloads Complete" --msgbox "Click Exit On The Main Menu Then Restart Emulation Station In Order To See Roms" 8 78
+            sudo sh downloader.sh
+        ;;
+        2)
+            exit
+        ;;
+        3)
             sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/nes.sh
             sudo bash ./nes.sh
             sudo rm -r nes.sh
             cd /home/pi/RomDownloader/Temp
             sudo sh gamelist.sh
         ;;
-        2)
-            echo "https://archive.org/download/gb_20201207/Complete%20Rom%20Sets/snes.zip" >> itemlist.txt
-            sudo sh ./decide.sh
-        ;;
-        3)
-            echo "https://archive.org/download/gb_20201207/Complete%20Rom%20Sets/gb.zip" >> itemlist.txt
-            sudo sh ./decide.sh
-        ;;
         4)
-            echo "https://archive.org/download/gb_20201207/Complete%20Rom%20Sets/gbc.zip" >> itemlist.txt
-            sudo sh ./decide.sh
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/snes.sh
+            sudo bash ./snes.sh
+            sudo rm -r snes.sh
+            cd /home/pi/RomDownloader/Temp
+            sudo sh gamelist.sh
         ;;
         5)
-            echo "https://archive.org/download/gb_20201207/games/gba.zip" >> itemlist.txt
-            sudo sh ./decide.sh
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/n64.sh
+            sudo bash ./n64.sh
+            sudo rm -r n64.sh
+            cd /home/pi/RomDownloader/Temp
+            sudo sh gamelist.sh
         ;;
         6)
-            exit
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/nds.sh
+            sudo bash ./nds.sh
+            sudo rm -r nds.sh
+            cd /home/pi/RomDownloader/Temp
+            sudo sh gamelist.sh
         ;;
         7)
-            echo "https://archive.org/download/gb_20201207/games/nds.zip" >> itemlist.txt
-            sudo sh ./decide.sh
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/gba.sh
+            sudo bash ./gba.sh
+            sudo rm -r gba.sh
+            cd /home/pi/RomDownloader/Temp
+            sudo sh gamelist.sh
         ;;
         8)
-            echo "https://archive.org/download/gb_20201207/Complete%20Rom%20Sets/virtualboy.zip" >> itemlist.txt
-            sudo sh ./decide.sh
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/nes.sh
+            sudo bash ./nes.sh
+            sudo rm -r nes.sh
+            cd /home/pi/RomDownloader/Temp
+            sudo sh gamelist.sh
         ;;
         9)
-            echo "https://archive.org/download/gb_20201207/games/megadrive.zip" >> itemlist.txt
-            sudo sh ./decide.sh
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/nds.sh
+            sudo bash ./nds.sh
+            sudo rm -r nds.sh
+            cd /home/pi/RomDownloader/Temp
+            sudo sh gamelist.sh
         ;;
         10)
-            echo "https://archive.org/download/gb_20201207/games/saturn.zip" >> itemlist.txt
-            sudo sh ./decide.sh
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/virtualboy.sh
+            sudo bash ./virtualboy.sh
+            sudo rm -r virtualboy.sh
+            cd /home/pi/RomDownloader/Temp
+            sudo sh gamelist.sh
         ;;
         11)
-            exit
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/megadrive.sh
+            sudo bash ./megadrive.sh
+            sudo rm -r megadrive.sh
+            cd /home/pi/RomDownloader/Temp
+            sudo sh gamelist.sh
         ;;
         12)
-            exit
+
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/saturn.sh
+            sudo bash ./saturn.sh
+            sudo rm -r saturn.sh
+            cd /home/pi/RomDownloader/Temp
+            sudo sh gamelist.sh
         ;;
         13)
-            exit
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/dreamcast.sh
+            sudo bash ./dreamcast.sh
+            sudo rm -r dreamcast.sh
+            cd /home/pi/RomDownloader/Temp
+            sudo sh gamelist.sh
         ;;
         14)
-            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/Download%20The%20Items%20You%20Selected.sh
-            sudo chmod +x "Download The Items You Selected.sh"
-            sudo sh "Download The Items You Selected.sh"
-            whiptail --title "Downloads Complete" --msgbox "Click Exit On The Main Menu Then Restart Emulation Station In Order To See Roms" 8 78
-            sudo sh downloader.sh
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/psx.sh
+            sudo bash ./psx.sh
+            sudo rm -r psx.sh
+            cd /home/pi/RomDownloader/Temp
+            sudo sh gamelist.sh
         ;;  
+        15) 
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/psp.sh
+            sudo bash ./psp.sh
+            sudo rm -r psp.sh
+            cd /home/pi/RomDownloader/Temp
+            sudo sh gamelist.sh
+        ;;
     esac
 }
 advancedMenu
