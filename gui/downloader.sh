ab1b2387f967d3c8da926e20082b3bc33b8d1cc6
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2020-12-24 23:32:30 -0500

    Update downloader.sh

--- gui/downloader.sh
+++ gui/downloader.sh
@@ -54,12 +54,32 @@ function advancedMenu {
         4) 
         whiptail --title "Media Downloads" --infobox "With this you will be able to download movies and tv shows found online. Only shows and movies with the traditional https encryption will be availible and a lot will b e lower quality" 8 78
         
+        #from here on Im seeing if it will work decently if i put it here
+        
+function advancedMenu {
+    ADVSEL=$(whiptail --title "What Do You Want To Download" --menu "Choose an option" 15 60 4 \
+        "1" "Movies" \
+        "2" "Tv" 3>&1 1>&2 2>&3)
+    case $ADVSEL in
+        1)
+        exit    
+        ;;
+        2)
+        exit
+        ;;
+    esac
+}
+advancedMenu
+
+        ;;
+        
+        
         5) 
         whiptail --title "Options Menu" --infobox "Not availible yet" 8 7
-        
+        ;;
         6) 
         whiptail --title "About" --infobox "This was orignally just meant for my portable pi project, but I might was make this public so here we are" 8 7
-        
+        ;;
         
         7)
             cd /home/pi/RomDownloader/Temp
